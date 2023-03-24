e2a488d3de01c0264616414a842883c3a50c8c1b
deanta-vinothkumar <73816271+deanta-vinothkumar@users.noreply.github.com>
2023-03-24 19:53:19 +0530

    MB-01 -feat: remove the comments

--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,7 +1,6 @@
 import './Card.css';
 
 const Card = (props) => {
-    // Testing Cards
     const classes = 'card ' + props.className;
     return <div className={classes}>{props.children}</div>
 }
